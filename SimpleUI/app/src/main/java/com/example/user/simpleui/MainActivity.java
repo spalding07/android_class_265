0a72723af4be02befeb3ef5095589163e941aa91
spalding07 <c0572@yahoo.com.tw>
2016-04-21 20:54:42 +0800

    implement text input and add radio group

--- SimpleUI/app/src/main/java/com/example/user/simpleui/MainActivity.java
+++ SimpleUI/app/src/main/java/com/example/user/simpleui/MainActivity.java
@@ -2,23 +2,50 @@ package com.example.user.simpleui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView textView;
+    EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textView = (TextView)findViewById(R.id.textView);
-        //textView.setText("Android Class");
+        editText = (EditText)findViewById(R.id.editText);
+
+        editText.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if(keyCode == KeyEvent.KEYCODE_ENTER && event.getAction() == KeyEvent.ACTION_DOWN){
+                    click(v);
+                    return true;    //true -> 攔截下來，結束，不會回傳到EditText上面  ； false 會繼續執行，介面就會接到 Enter
+                }
+                return false;
+            }
+        });
+
+        editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if( actionId == EditorInfo.IME_ACTION_DONE){    //介面要設定single line，不然enter的意思是換行
+                    click(v);
+                    return true;
+                }
+                return false;
+            }
+        });
     }
 
     public void click(View view){
-        //textView.setText("Android Class");
-        textView.setText("Android Class");
+        String text = editText.getText().toString();
+        textView.setText(text);
+        editText.setText("");
     }
 }
