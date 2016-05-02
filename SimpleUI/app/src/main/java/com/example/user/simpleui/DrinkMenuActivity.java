19787327c5369ce7eeb298c2f4e1ba6948496591
spalding07 <c05725@yahoo.com.tw>
2016-05-02 21:01:57 +0800

    implement getData and add

---
+++ SimpleUI/app/src/main/java/com/example/user/simpleui/DrinkMenuActivity.java
@@ -0,0 +1,105 @@
+package com.example.user.simpleui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class DrinkMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drink_menu);
+        Log.d("debug", "DrinkMenuActivity OnCreate");
+    }
+
+    public void add(View view) {
+        Button button = (Button) view;
+        String text = button.getText().toString();
+        int count = Integer.parseInt(text);
+        count++;
+        button.setText(String.valueOf(count));
+    }
+
+    public void cancel(View view) {
+        finish();   //結束現在的activity，會直接destory掉
+    }
+
+    public void done(View view) {   //紀錄介面所有的杯數，傳回到前一頁
+    }
+
+    public JSONArray getData() {
+        LinearLayout rootLinearLayout = (LinearLayout) findViewById(R.id.root);
+
+        JSONArray jsonArray = new JSONArray();
+
+        for (int i = 1; i < 4; i++) {
+            LinearLayout linearLayout = (LinearLayout) rootLinearLayout.getChildAt(i);
+
+            TextView textView = (TextView) linearLayout.getChildAt(0);
+            Button mButton = (Button) linearLayout.getChildAt(1);
+            Button lButton = (Button) linearLayout.getChildAt(2);
+
+            String drinkName = textView.getText().toString();
+            int m = Integer.parseInt(mButton.getText().toString());
+            int l = Integer.parseInt(textView.getText().toString());
+
+            JSONObject jsonObject = new JSONObject();
+            try {
+                jsonObject.put("name", drinkName);
+                jsonObject.put("m", m);
+                jsonObject.put("l", l);
+
+                jsonArray.put(jsonObject);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        return jsonArray;
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("debug", "DrinkMenuActivity OnStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("debug", "DrinkMenuActivity OnResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("debug", "DrinkMenuActivity OnPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("debug", "DrinkMenuActivity OnStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("debug", "DrinkMenuActivity OnDestroy");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("debug", "DrinkMenuActivity OnRestart");
+    }
+}
--- SimpleUI/app/src/main/java/com/example/user/simpleui/MainActivity.java
+++ SimpleUI/app/src/main/java/com/example/user/simpleui/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.user.simpleui;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
@@ -45,6 +47,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("debug", "Main Activity On Created");
         textView = (TextView) findViewById(R.id.textView);
         editText = (EditText) findViewById(R.id.editText);
         radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
@@ -88,21 +91,21 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        int checkedId = sp.getInt("radioGroup", R.id.blackTeaRadioButton);
-        radioGroup.check(checkedId);
-
-        RadioButton radioButton = (RadioButton) findViewById(checkedId);
-        drinkName = radioButton.getText().toString();
-
-        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
-                RadioButton radioButton = (RadioButton) findViewById(checkedId);
-                editor.putInt("radioGroup", checkedId);
-                editor.apply();
-                drinkName = radioButton.getText().toString();
-            }
-        });
+//        int checkedId = sp.getInt("radioGroup", R.id.blackTeaRadioButton);
+//        radioGroup.check(checkedId);
+//
+//        RadioButton radioButton = (RadioButton) findViewById(checkedId);
+//        drinkName = radioButton.getText().toString();
+//
+//        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+//            @Override
+//            public void onCheckedChanged(RadioGroup group, int checkedId) {
+//                RadioButton radioButton = (RadioButton) findViewById(checkedId);
+//                editor.putInt("radioGroup", checkedId);
+//                editor.apply();
+//                drinkName = radioButton.getText().toString();
+//            }
+//        });
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -168,5 +171,49 @@ public class MainActivity extends AppCompatActivity {
         editText.setText("");
 
         setupListView();
+    }
+
+    public void goToMenu(View view){
+        Intent intent = new Intent();   //呼叫Activity的媒介
+
+        intent.setClass(this, DrinkMenuActivity.class);
+
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("debug", "Main Activity OnStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("debug", "Main Activity OnResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("debug", "Main Activity OnPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("debug", "Main Activity OnStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("debug", "Main Activity OnDestroy");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("debug", "Main Activity OnRestart");
     }
 }
